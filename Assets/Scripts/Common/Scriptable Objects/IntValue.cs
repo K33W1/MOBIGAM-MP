69e254ba2438a149a1ddcec06323314511a18256
K33W1 <bruceakimiranda@gmail.com>
2020-10-23 21:32:59 +0800

    Added score UI

--- Assets/Scripts/Common/Scriptable Objects/IntValue.cs
+++ Assets/Scripts/Common/Scriptable Objects/IntValue.cs
@@ -1,13 +1,20 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Int Value", menuName = "Common/IntValue")]
 public class IntValue : ScriptableObject
 {
     [SerializeField] private int value = 0;
 
+    public event Action<int> ValueChanged;
+
     public int Value
     {
         get => value;
-        set => this.value = value;
+        set
+        {
+            this.value = value;
+            ValueChanged?.Invoke(value);
+        }
     }
 }
---
+++ Assets/Scripts/UI/ScoreText.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ScoreText : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private IntValue stageScore = null;
+
+    private TextMeshProUGUI text = null;
+
+    private void Awake()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        stageScore.ValueChanged += i => text.text = i.ToString().PadLeft(6, '0');
+    }
+}
