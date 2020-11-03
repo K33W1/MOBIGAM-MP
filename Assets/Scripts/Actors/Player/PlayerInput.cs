59ced9eda6d50e92da8b58889dd9afc29f987b4c
K33W1 <bruceakimiranda@gmail.com>
2020-11-03 12:40:41 +0800

    Changed input system to old
    New input system does not support Unity Remote

--- Assets/Scripts/Actors/Player/PlayerInput.cs
+++ Assets/Scripts/Actors/Player/PlayerInput.cs
@@ -12,32 +12,48 @@ public class PlayerInput : MonoBehaviour
     public event Action SwitchWeaponUp;
     public event Action SwitchWeaponDown;
 
-    private InputMaster input = null;
-
     public Vector2 Move { get; private set; }
-
-    private void Awake()
-    {
-        input = new InputMaster();
-        input.Player.StartFire.performed += _ => StartFire?.Invoke();
-        input.Player.StopFire.performed += _ => StopFire?.Invoke();
-        input.Player.SwitchUp.performed += _ => SwitchWeaponUp?.Invoke();
-        input.Player.SwitchDown.performed += _ => SwitchWeaponDown?.Invoke();
-    }
-
-    private void OnEnable() => input.Enable();
 
     private void Update()
     {
-        Vector2 keyboardMove = input.Player.KeyboardMove.ReadValue<Vector2>();
-        Vector2 joystickMove = input.Player.JoystickMove.ReadValue<Vector2>();
+        FireCheck();
+        SwitchWeaponCheck();
+        MoveCheck();
+    }
 
-        Vector2 rawMove = keyboardMove + joystickMove;
+    private void FireCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartFire?.Invoke();
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            StopFire?.Invoke();
+        }
+    }
+
+    private void SwitchWeaponCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            SwitchWeaponDown?.Invoke();
+        }
+        else if (Input.GetKeyUp(KeyCode.E))
+        {
+            SwitchWeaponUp?.Invoke();
+        }
+    }
+
+    private void MoveCheck()
+    {
+        float rawX = Input.GetAxisRaw("Horizontal");
+        float rawY = Input.GetAxisRaw("Vertical");
+
+        Vector2 rawMove = new Vector2(rawX, rawY);
         Vector2 normalizedMove = rawMove.sqrMagnitude > 1f ? rawMove.normalized : rawMove;
 
         float smoothing = moveSmoothing * Time.deltaTime;
         Move = Vector2.Lerp(Move, normalizedMove, smoothing);
     }
-
-    private void OnDisable() => input.Disable();
 }
