5b8d85910181e3617436e5cc21042b5b20e06a79
K33W1 <bruceakimiranda@gmail.com>
2020-11-08 20:22:57 +0800

    Moved player's colliders to the mesh gameobject

---
+++ Assets/_Project/Scripts/Actors/Player/DisableColliderOnDeath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Health))]
+public class DisableColliderOnDeath : MonoBehaviour
+{
+    private List<Collider> colliders = new List<Collider>();
+
+    private void Awake()
+    {
+        Health health = GetComponent<Health>();
+
+        foreach (Collider collider in GetComponentsInChildren<Collider>())
+        {
+            health.Died += () => collider.enabled = false;
+        }
+    }
+}
