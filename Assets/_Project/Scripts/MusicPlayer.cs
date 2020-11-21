03261a9946ad8d2ba9c15a6c6ad0b466d612c4ae
K33W1 <bruceakimiranda@gmail.com>
2020-11-21 22:13:14 +0800

    Added music

---
+++ Assets/_Project/Scripts/MusicPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class MusicPlayer : MonoBehaviour
+{
+    private AudioSource audioSource = null;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        audioSource.Play();
+    }
+}
